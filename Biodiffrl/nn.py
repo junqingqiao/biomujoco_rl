d8caeac32839081192919dae6715feff3958002c
junqingqiao <junqingqiaoustc@gmail.com>
2024-11-12 10:57:03 -0500

    Developing learning class and added aot compiler

---
+++ Biodiffrl/nn.py
@@ -0,0 +1,74 @@
+import jax
+import jax.numpy as jp
+import flax
+import flax.linen as nn
+import optax
+
+
+class Controller_NN(nn.Module):
+    
+    in_dims: int
+    out_dims: int
+    
+    def setup(self):
+        # Features means the output dimension
+        self.linear1 = nn.Dense(features=400)
+        self.linear2 = nn.Dense(features=400)
+        self.linear3 = nn.Dense(features=400)
+        self.linear4 = nn.Dense(features=400)
+        # The last layer will output the mean and logstd
+        self.linear5 = nn.Dense(features=self.out_dims*2)
+        
+    
+    def __call__(self, x, key):
+        x = self.linear1(x)
+        x = nn.relu(x)
+        x = self.linear2(x)
+        x = nn.relu(x)
+        x = self.linear3(x)
+        x = nn.relu(x)
+        x = self.linear4(x)
+        x = nn.relu(x)
+        x = self.linear5(x)
+        # The last layer of the neural requires samping
+        mean = x[:self.out_dims]
+        logstd = x[self.out_dims:]
+        std = jp.exp(logstd)
+        # samples = jp.clip(jax.random.normal(key)*std*0.3 + mean, -3, 3)
+        samples = jax.random.normal(key)*std*0.3 + mean
+        return samples, mean, logstd
+    
+    def init_parameters(self, key):
+        # Init the model
+        sub_key = jax.random.split(key,1)[0]
+        # The second parameter is the dommy input
+        params = self.init(key, jp.empty([1, self.in_dims]), sub_key)
+        return params, sub_key
+    
+    def get_fn(self):
+        return lambda params, states, key : self.apply(params, states, key)
+    
+class Critic_NN(nn.Module):
+
+    def setup(self):
+        # Features means the output dimension
+        self.linear1 = nn.Dense(features=400)
+        self.linear2 = nn.Dense(features=400)
+        self.linear3 = nn.Dense(features=400)
+        self.linear4 = nn.Dense(features=400)
+        # The last layer will output the mean and logstd
+        self.linear5 = nn.Dense(features=1)
+        
+    
+    def __call__(self, x):
+        x = self.linear1(x)
+        x = nn.relu(x)
+        x = self.linear2(x)
+        x = nn.relu(x)
+        x = self.linear3(x)
+        x = nn.relu(x)
+        x = self.linear4(x)
+        x = nn.relu(x)
+        x = self.linear5(x)
+        # The last layer of the neural requires samping
+        return -nn.relu(x)
